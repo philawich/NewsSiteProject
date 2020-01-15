17fc0a6116fed43fc34de0e49f7cae09934355a8
Phillip <phil.awich@gmail.com>
2020-01-15 12:22:07 -0500

    Add toggle dark mode in JS file

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", ()=> {
+    let toggleDark = document.querySelector("#moon")
     
+    toggleDark.addEventListener("click", (e) => {
+        e.preventDefault()
+        document.body.style.background = "#080808";
+        document.body.style.color = "white";
+
+
+        
+        // if (img.value === "Day") {
+        //     body.style.background = "black"
+        //     img.src = "./sun.png" 
+        // } else {
+        //     // opposite
+        // }
+
+    })
+    
+
 })
+
+
+
